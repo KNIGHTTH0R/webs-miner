60c7791d05fc21c75fd931e5ffdd97df9a47cb84
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-15 22:24:10 -0400

    Added source directory with the main file.

---
+++ src/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Extract information from a web page and save in a database then you can visualized in a char."""
+
+if __name__ == "__main__":
+    pass
