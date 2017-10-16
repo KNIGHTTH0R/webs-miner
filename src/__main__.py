55f2148e4eb5714e75c91ba5f631dc3eac40d82a
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-16 00:27:05 -0400

    Added the main struct for the main file.

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-"""Extract information from a web page and save in a database then you can visualized in a char."""
+"""Extract information from a web page and save in a database then you can visualized in a char.
+Download the last version of the pkg modules which is my personal library.
+https://raw.githubusercontent.com/airvzxf/python-packages/master/versions/pkg_last_version.tar.gz
+"""
 
 if __name__ == "__main__":
+    pass
+
+
+def _download_pkg_modules():
+    pass
+
+
+def _extract_pkg_modules():
+    pass
+
+
+def _delete_pkg_downloaded_file():
     pass
