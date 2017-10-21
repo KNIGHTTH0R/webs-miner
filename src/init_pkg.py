aa417013ae2dabb1eb37639b074711492a14eed9
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-21 01:43:59 -0400

    Added the logic to check if the pkg folder exist if not exist it try to install.

--- src/init_pkg.py
+++ src/init_pkg.py
@@ -3,7 +3,19 @@
 
 """Download and init the external pkg"""
 
-from init_packages.init_packages import initialize
+from os.path import dirname, exists, realpath
 
-if __name__ == "__main__":
-    initialize()
+import init_packages
+
+
+def initialize(force_init=False):
+    """If the pkg folder not exist it download the new one but if if force_init it always download and init
+     the external package.
+     """
+    _current_directory = dirname(realpath(__file__))
+
+    if not exists(_current_directory + '/pkg/'):
+        init_packages.init_packages.initialize(delete_pkg=True)
+
+    if force_init:
+        init_packages.init_packages.initialize(delete_pkg=True)
