8ca31abbddfcae61e8ce5c131ef56ed14bb2937a
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-21 14:08:15 -0400

    Imported yaml and read and print our fitst config file (web-links).

--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,7 @@
 """Main/Initial script for extract the data from the web sites."""
 
 import init_pkg
+import yaml
 
 init_pkg.initialize()
 
@@ -16,28 +17,8 @@ if __name__ == "__main__":
     # https://stackoverflow.com/questions/6866600/yaml-parsing-and-python
     # YAML is better for config files vs JSON.
     print("get_open: ", pkg.html.tag.regex.get_open.get_open())
-    pass
+    with open('./config/web_links.yaml') as yaml_file:
+        web_links_list_config = yaml.load(yaml_file)
 
-# First I need a list with this data.
-# ----------------------------------------------------------------------
-# Link: https://www.bose.com/en_us/products/headphones/over_ear_headphones/quietcomfort-35-wireless-ii.html
-# Brand: BOSE
-# Model: Quiet comfort 35 Wireless II / Black
-# SKU: 789564
-# Category: Headphones
-# Sub-category: Wireless
-# ----------------------------------------------------------------------
-# Link: http://store.asus.com/us/item/201702AM030000001
-# Brand: ASUS
-# Model: ROG G701VI-XS72K OC Edition
-# SKU: 201702AM030000001
-# Category: Laptop
-# Sub-category: None
-# ----------------------------------------------------------------------
-# Link: https://store.asus.com/us/item/201701AM100000021
-# Brand: ASUS
-# Model: ROG Ranger Backpack
-# SKU: 201701AM100000021
-# Category: Laptop
-# Sub-category: Backpack
-# ----------------------------------------------------------------------
+    for web_page in web_links_list_config:
+        print(web_page)
