e56f81e0db7ea8560a57f2ac40f049ddc5206085
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-21 00:55:48 -0400

    Added script to init the expternal pkg, the user needs to execute manually, per default always delete the current pkg file and directory but you can't avoid that with a parameter in the function.

---
+++ src/init_pkg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""Download and init the external pkg"""
+
+from init_packages.init_packages import initialize
+
+if __name__ == "__main__":
+    initialize()
