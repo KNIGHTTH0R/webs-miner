ebbddbe139613de0df1e779a11fdfbf6d76d5f7e
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-29 12:40:18 -0400

    Updated the TODO list, create a file with constants is done.

--- src/__main__.py
+++ src/__main__.py
@@ -12,9 +12,8 @@ import pkg
 # noinspection PyPep8
 from core.setup.get_web_links import get_web_links
 
-# TODO: 1. Move all the generic constant to one file.
-# TODO: 2. Create test files for the current modules.
-# TODO: 3. Move into core init, This file only should be contains init_pkg.initialize(...) and pkg.start()
+# TODO: 1. Create test files for the current modules.
+# TODO: 2. Move into core init, This file only should be contains init_pkg.initialize(...) and pkg.start()
 if __name__ == "__main__":
     get_web_links()
     print("get_open: ", pkg.html.tag.regex.get_open.get_open())
