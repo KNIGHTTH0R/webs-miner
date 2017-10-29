8d1b85914880e46b032c5ef0fe459141ceb27f4e
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-29 11:32:57 -0400

    Rename the folder config to preferences, much better.

--- src/core/config/__init__.py
+++ src/core/config/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-"""Get all the values in config files."""
+"""Get all the values in preferences files."""
 
 from . import get_web_links
--- src/core/config/get_web_links.py
+++ src/core/config/get_web_links.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-"""Extract the web links into the config file and return a list."""
+"""Extract the web links into the preferences file and return a list."""
 
 from os.path import dirname, realpath
 
@@ -11,16 +11,16 @@ import yaml
 # TODO: Crete test for this file.
 def get_web_links():
     """
-    Get the web links from the yaml config file.
+    Get the web links from the yaml preferences file.
     """
     _current_directory = dirname(realpath(__file__))
 
-    with open(_current_directory + '/../../config/web_links.yaml') as yaml_file:
+    with open(_current_directory + '/../../preferences/web_links.yaml') as yaml_file:
         web_links_list_config = yaml.load(yaml_file)
 
         if not web_links_list_config:
-            print('Error: web links config file is empty.')
-            print('The file web_links.yaml in ./config folder is empty. You need to add your web links')
+            print('Error: web links preferences file is empty.')
+            print('The file web_links.yaml in ./preferences folder is empty. You need to add your web links')
             exit(code=1)
 
         for web_page_key in web_links_list_config:
